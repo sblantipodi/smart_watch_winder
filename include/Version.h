174101e180015f9364679018c8e34685bb513546
Davide Perini <perini.davide@dpsoftware.org>
2020-07-14 22:47:47 +0200

    Continuous Integration, binary firmware is now uploaded on every release

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.8.24"
+        #define VERSION "2.9.6"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-07-09 21:34:11.929238"
+        #define BUILD_TIMESTAMP "2020-07-14 22:46:38.975284"
     #endif
     

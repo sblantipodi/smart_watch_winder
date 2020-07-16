cdc68dec6df0139c45d5e545c2e72267fc1e4114
Davide Perini <perini.davide@dpsoftware.org>
2020-07-16 22:12:27 +0200

    Updated Arduino core to 2.7.2

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.9.6"
+        #define VERSION "2.9.12"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-07-14 22:46:38.975284"
+        #define BUILD_TIMESTAMP "2020-07-16 22:10:48.444313"
     #endif
     

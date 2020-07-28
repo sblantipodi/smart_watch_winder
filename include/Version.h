d2850f135b0878fc53582dc0218dd7378a377a41
Davide Perini <perini.davide@dpsoftware.org>
2020-07-28 23:39:36 +0200

    Switch to JetBrains CLion

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.9.25"
+        #define VERSION "2.9.30"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-07-28 23:16:35.104261"
+        #define BUILD_TIMESTAMP "2020-07-28 23:35:41.104713"
     #endif
     

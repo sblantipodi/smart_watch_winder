5fe0f14505b3bf62fea5dfe1b8490fee389df1c8
Davide Perini <perini.davide@dpsoftware.org>
2023-09-25 12:38:45 +0200

    libs udpate

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.16.69"
+        #define VERSION "2.16.74"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-08-19 19:01:57.991295"
+        #define BUILD_TIMESTAMP "2023-09-25 12:33:01.967115"
     #endif
     

b8ee6ba4bc7b5e9d968d8c2dacdaaad54cf8ed3f
Davide Perini <perini.davide@dpsoftware.org>
2024-07-26 22:31:02 +0200

    Arduino Bootstrapper (v1.18.1)

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.18.36"
+        #define VERSION "2.18.41"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-07-05 11:27:43.806004"
+        #define BUILD_TIMESTAMP "2024-07-26 22:17:41.535176"
     #endif
     

b2cda36786059c2de0d01fe4316f7c71a2f81432
Davide Perini <perini.davide@dpsoftware.org>
2024-12-14 17:04:28 +0100

    esp32 s3 support

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.18.51"
+        #define VERSION "2.19.6"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-11-17 14:58:28.843617"
+        #define BUILD_TIMESTAMP "2024-12-14 17:03:38.776069"
     #endif
     

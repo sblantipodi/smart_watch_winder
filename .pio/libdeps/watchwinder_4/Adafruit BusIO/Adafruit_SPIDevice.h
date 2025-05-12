7e0f37680dea33e681f230cf260867a3e6d39b4c
Davide Perini <perini.davide@dpsoftware.org>
2025-05-12 13:23:21 +0200

    libs update

--- .pio/libdeps/watchwinder_4/Adafruit BusIO/Adafruit_SPIDevice.h
+++ .pio/libdeps/watchwinder_4/Adafruit BusIO/Adafruit_SPIDevice.h
@@ -56,7 +56,7 @@ typedef BitOrder BusIOBitOrder;
 // ports set and clear registers which are atomic.
 // typedef volatile uint32_t BusIO_PortReg;
 // typedef uint32_t BusIO_PortMask;
-//#define BUSIO_USE_FAST_PINIO
+// #define BUSIO_USE_FAST_PINIO
 
 #elif defined(__MBED__) || defined(__ZEPHYR__)
 // Boards based on RTOS cores like mbed or Zephyr are not going to expose the
@@ -79,7 +79,8 @@ typedef uint32_t BusIO_PortMask;
 
 #elif (defined(__arm__) || defined(ARDUINO_FEATHER52)) &&                      \
     !defined(ARDUINO_ARCH_RP2040) && !defined(ARDUINO_SILABS) &&               \
-    !defined(ARDUINO_UNOR4_MINIMA) && !defined(ARDUINO_UNOR4_WIFI)
+    !defined(ARDUINO_UNOR4_MINIMA) && !defined(ARDUINO_UNOR4_WIFI) &&          \
+    !defined(PORTDUINO)
 typedef volatile uint32_t BusIO_PortReg;
 typedef uint32_t BusIO_PortMask;
 #if !defined(__ASR6501__) && !defined(__ASR6502__)
--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.19.27"
+        #define VERSION "2.19.33"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-04-11 16:15:20.808466"
+        #define BUILD_TIMESTAMP "2025-05-12 13:21:56.950626"
     #endif
     

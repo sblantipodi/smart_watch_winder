d3c13d3b59fd769825b7f2ac7097954eeb925d96
Davide Perini <perini.davide@dpsoftware.org>
2026-01-02 00:19:52 +0100

    Copyright 2026 - Update

--- include/Watchwinder.h
+++ include/Watchwinder.h
@@ -1,7 +1,7 @@
 /*
   Watchwinder.cpp - Smart Watch Winder
   
-  Copyright © 2020 - 2024  Davide Perini
+  Copyright © 2020 - 2026  Davide Perini
   
   Permission is hereby granted, free of charge, to any person obtaining a copy of 
   this software and associated documentation files (the "Software"), to deal
